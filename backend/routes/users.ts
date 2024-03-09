2d8909307ca883fa0b520daa70f2517c53bc42ee
Hairum Qureshi <hqureshi@udel.edu>
2024-03-09 18:00:15 -0500

    just a test

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -3,7 +3,6 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import User from "../models/user";
 import User_Interface from "../interfaces";
 import Notebook from "../models/notebook";
-import Notebook_Interface from "../interfaces";
 
 const router = express.Router();
 
